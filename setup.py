e943b77d03ebfe9f63114b96a91c3b0f3e7232be
ThibTrip <40694343+ThibTrip@users.noreply.github.com>
2020-02-16 13:59:39 +0100

    Bump to version 1.3.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(here, "requirements.txt"),"r") as f:
 
 setuptools.setup(
     name="pangres",
-    version="1.3",
+    version="1.3.1",
     license = 'The Unlicense',
     author="Thibault Bétrémieux",
     author_email="thibault.betremieux@gmail.com",
