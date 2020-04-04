5ad1521f503cbc1e6003a75886d86e3b2e5f7f1d
ThibTrip <40694343+ThibTrip@users.noreply.github.com>
2020-04-04 19:16:35 +0200

    update tests

--- pangres/tests/conftest.py
+++ pangres/tests/conftest.py
@@ -1,31 +1,54 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-Configuration of the tests of pangres for pytest.
-
-The class TestDB configures the database. It is set
-to work with CircleCI.
-
-You may change the connection string and the schema in class TestDB
-temporarily for local testing but **be warned**: 
-1) All tables in the schema will be wiped!
-2) Please use a local database with dummy credentials
-   to avoid accidentaly commiting private information!
+Configuration and helpers for the tests of pangres with pytest.
 """
+import pandas as pd
+import json
 from sqlalchemy import create_engine
 
-class TestDB:
-    connection_string = "postgresql://circleci_user:password@localhost:5432/circleci_test?sslmode=disable"
-    schema = 'tests'
-    engine = create_engine(connection_string)
+# # Helpers
+
+def drop_table_if_exists(engine, schema, table_name):
+    namespace = f'{schema}.{table_name}' if schema is not None else table_name
+    engine.execute(f'DROP TABLE IF EXISTS {namespace};')
 
 
-def pytest_sessionstart(session):
-    """
-    Called after the Session object has been created and
-    before performing collection and entering the run test loop.
-    """
-    testdb = TestDB()
-    # clean schema
-    testdb.engine.execute(f'DROP SCHEMA IF EXISTS {testdb.schema} CASCADE;')
-    testdb.engine.execute(f'CREATE SCHEMA {testdb.schema}')
+# ## Class TestDB
+
+# +
+def pytest_addoption(parser):
+    parser.addoption('--conn_string', action="store")
+    parser.addoption('--schema', action='store', default=None)
+
+
+def pytest_generate_tests(metafunc):
+    # This is called for every test. Only get/set command line arguments
+    # if the argument is specified in the list of test "fixturenames".
+    conn_string = metafunc.config.option.conn_string
+    engine = create_engine(conn_string)
+    schema = metafunc.config.option.schema
+    metafunc.parametrize("engine", [engine])
+    metafunc.parametrize("schema", [schema])
+
+
+# -
+
+# ## Function to read back from database data we inserted
+# We need to apply a few modification for comparing DataFrames we get back from the DB and DataFrames we expect e.g. for JSON (with SQlite pandas reads it as string).
+
+def read_example_table_from_db(engine, schema, table_name):
+    def load_json_if_needed(obj):
+        """
+        For SQlite we receive strings back (or None) for a JSON column.
+        For Postgres we receive lists or dicts (or None) back.
+        """
+        if isinstance(obj, str):
+            return json.loads(obj)
+        return obj
+    namespace = f'{schema}.{table_name}' if schema is not None else table_name
+    df_db = (pd.read_sql(f'SELECT * FROM {namespace}', con=engine, index_col='profileid')
+             .astype({'likes_pizza':bool})
+             .assign(timestamp=lambda df: pd.to_datetime(df['timestamp'], utc=True))
+             .assign(favorite_colors= lambda df: df['favorite_colors'].map(load_json_if_needed)))
+    return df_db
--- pangres/tests/test_index.py
+++ pangres/tests/test_index.py
@@ -1,115 +1,70 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""
+Here we test different indices:
+1. MultiIndex
+2. Indices with NULL
 
-
-
-from sqlalchemy import create_engine
-from sqlalchemy.exc import IntegrityError
+# Note
+Other problems with indices such as duplicated values are handled by pangres.helpersPandasSpecialEngine.
+"""
 import pandas as pd
-import numpy as np
-import datetime
-import psycopg2
-import random
-import os
-
-from pangres import pg_upsert
-from pangres.tests.conftest import TestDB
+from sqlalchemy import VARCHAR
+from sqlalchemy.exc import IntegrityError
+from pangres import upsert
+from pangres.tests.conftest import drop_table_if_exists
 
 
 # # Config
 
+# +
+# for creating table with MultiIndex
+index_col = ['ix1', 'ix2', 'ix3']
+df_multiindex = pd.DataFrame({'ix1': [1, 1], 'ix2': ['test', 'test2'], 
+                              'ix3': [pd.Timestamp('2019-01-01'), pd.Timestamp('2019-01-02')],
+                              'foo': [1, 2]}).set_index(index_col)
 
-testdb = TestDB()
+# for inserting values
+df_multiindex2 = pd.DataFrame({'ix1': [2, 2], 'ix2': ['test', 'test2'], 
+                              'ix3': [pd.Timestamp('2019-01-01'), pd.Timestamp('2019-01-02')],
+                              'foo': [1, 2]}).set_index(index_col)
 
+default_args = {'if_row_exists':'update',
+                'adapt_dtype_of_empty_db_columns':False}
+
+
+# -
 
 # # Tests
 
+# ## Test MultiIndex
 
+def test_create_and_insert_table_multiindex(engine, schema):
+    table_name = 'test_multiindex'
+    namespace = f'{schema}.{table_name}' if schema is not None else table_name
+    drop_table_if_exists(engine=engine, schema=schema, table_name=table_name)
+    # dtype for index for MySQL... (can't have flexible text length)
+    dtype = {'ix2':VARCHAR(5)} if 'mysql' in engine.dialect.dialect_description else None
 
-def test_create_and_insert_table_multiindex():
     # create
-    df = pd.DataFrame({
-    'ix1': [1, 1],
-    'ix2': ['test', 'test2'],
-    'ix3': [pd.Timestamp('2019-01-01'),
-            pd.Timestamp('2019-01-02')],
-    'foo': [1, 2]
-    }).set_index(['ix1', 'ix2', 'ix3'])
-
-    pg_upsert(engine=testdb.engine,
-              df=df,
-              schema=testdb.schema,
-              table_name='test_multiindex',
-              if_exists='upsert_overwrite',
-              create_schema=True,
-              add_new_columns=True,
-              adapt_dtype_of_empty_db_columns=True)
-
-    df_db = pd.read_sql(f'SELECT * FROM {testdb.schema}.test_multiindex',
-                        con=testdb.engine,
-                        index_col=['ix1', 'ix2', 'ix3'])
-    pd.testing.assert_frame_equal(df, df_db)
+    upsert(engine=engine, schema=schema, df=df_multiindex, table_name=table_name, dtype=dtype, **default_args)
+    df_db = pd.read_sql(f'SELECT * FROM {namespace}', con=engine, index_col=index_col)
 
     # insert
-    df2 = pd.DataFrame({
-        'ix1': [2, 2],
-        'ix2': ['test', 'test2'],
-        'ix3': [pd.Timestamp('2019-01-01'),
-                pd.Timestamp('2019-01-02')],
-        'foo': [1, 2]
-    }).set_index(['ix1', 'ix2', 'ix3'])
-
-    pg_upsert(engine=testdb.engine,
-              df=df2,
-              schema=testdb.schema,
-              table_name='test_multiindex',
-              if_exists='upsert_overwrite',
-              create_schema=True,
-              add_new_columns=True,
-              adapt_dtype_of_empty_db_columns=True)
-
-    df_db = pd.read_sql(f'SELECT * FROM {testdb.schema}.test_multiindex',
-                        con=testdb.engine,
-                        index_col=['ix1', 'ix2', 'ix3'])
-    pd.testing.assert_frame_equal(pd.concat([df, df2]), df_db)
+    upsert(engine=engine, schema=schema, df=df_multiindex2, table_name=table_name, dtype=dtype, **default_args)
+    df_db = pd.read_sql(f'SELECT * FROM {namespace}', con=engine, index_col=index_col)
 
 
-def test_index_with_null():
-    df3 = pd.DataFrame({
-        'ix1': [None, None],
-        'ix2': ['test', 'test2'],
-        'ix3': [pd.Timestamp('2019-01-01'),
-                pd.Timestamp('2019-01-02')],
-        'foo': [1, 2]
-    }).set_index(['ix1', 'ix2', 'ix3'])
+# ## Test index with null value
 
-    try:
-        pg_upsert(engine=testdb.engine,
-                  df=df3,
-                  schema=testdb.schema,
-                  table_name='test_index_with_null',
-                  if_exists='upsert_overwrite',
-                  create_schema=True,
-                  add_new_columns=True,
-                  adapt_dtype_of_empty_db_columns=True)
-    except IntegrityError as e:
-        print((f'Insert of a DataFrame with null '
-               f'values in index failed as expected. Error was:\n\n{e}'))
-
-def test_non_unique_index():
-    df_non_unique_ix = (pd.DataFrame({'profileid':[1,1],
-                                      'date':['2018-01-01','2018-01-01'],
-                                      'favorite_fruit':['banana','apple']})
-                        .set_index(['profileid','date']))
-    try:
-        pg_upsert(engine=testdb.engine,
-                  df=df_non_unique_ix,
-                  schema=testdb.schema,
-                  table_name='fail_duplicated_index',
-                  if_exists='upsert_overwrite',
-                  create_schema=True,
-                  add_new_columns=True,
-                  adapt_dtype_of_empty_db_columns=True)
-    except IndexError as e:
-        print((f'Insert of a DataFrame with non unique index '
-               f'values in index failed as expected. Error was:\n\n{e}'))
+def test_index_with_null(engine, schema):
+    df = pd.DataFrame({'ix':[None], 'foo': [2]}).set_index('ix')
+    table_name='test_index_with_null'
+    drop_table_if_exists(engine=engine, schema=schema, table_name=table_name)
+    # don't test for mysql since only a warning is raised and the line is skipped
+    if not 'mysql' in engine.dialect.dialect_description:
+        try:
+            upsert(engine=engine, schema=schema, df=df, table_name=table_name, **default_args)
+            raise ValueError('upsert did not fail as expected with null value in index')
+        except IntegrityError as e:
+            print(f'upsert failed as expected with null value in index. Error was:\n\n{e}')
