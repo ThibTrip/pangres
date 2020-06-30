95575c6e1ea0a01e0d50aff2230e5b247fccf51e
ThibTrip <thibault.betremieux@port-neo.com>
2020-06-30 21:18:12 +0200

    modify structure once again to avoid issues on pangres load

--- pangres/docs/generate_documentation.py
+++ pangres/docs/generate_documentation.py
@@ -7,14 +7,18 @@ This script generates the documentation of pangres using the library [npdoc_to_m
 to render markdown files (located in the same folder as this notebook) with Jinja like placeholders (docstrings of functions,
 methods or classes are pulled and converted to pretty markdown).
 """
+
+
 # -
 
 # # Parse arguments
 
-parser = argparse.ArgumentParser(description=script_description)
-parser.add_argument('wiki_path', metavar='wiki_path', type=str, help="Path to pangres' cloned wiki folder on your computer")
-args = parser.parse_args()
-wiki_path = args.wiki_path
+def parse_arguments():
+    parser = argparse.ArgumentParser(description=script_description)
+    parser.add_argument('wiki_path', metavar='wiki_path', type=str, help="Path to pangres' cloned wiki folder on your computer")
+    args = parser.parse_args()
+    wiki_path = args.wiki_path
+    return wiki_path
 
 
 # # Render markdown files
@@ -22,7 +26,7 @@ wiki_path = args.wiki_path
 # Each Markdown file corresponds to a wiki page except README.md
 
 # +
-def main():
+def generate_doc(wiki_path):
     from npdoc_to_md import render_md_file, get_markdown_files_in_dir
 
     files_and_names = get_markdown_files_in_dir('.')
@@ -38,5 +42,6 @@ def main():
     print('Done')
 
 if __name__ == '__main__':
-    main()
+    wiki_path = parse_arguments()
+    generate_doc(wiki_path)
 
