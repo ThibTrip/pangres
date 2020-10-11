0155891e08d0ad1747be0c6806e5204027a909d6
Thibault <thibault.betremieux@port-neo.com>
2020-10-11 23:34:49 +0200

    bump to version 2.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(here, "requirements.txt"),"r") as f:
 
 setuptools.setup(
     name="pangres",
-    version="2.2",
+    version="2.2.1",
     license = 'The Unlicense',
     author="Thibault Bétrémieux",
     author_email="thibault.betremieux@gmail.com",
