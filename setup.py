1eba21ae1c6bf16ddc2db710c0d98d46caeb9afc
SuJiKiNen <sujikinen@gmail.com>
2021-04-28 23:28:55 -0400

    Pin sqlalchemy version less than 1.3.24

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ with open(os.path.join(here, "requirements.txt"),"r") as f:
 
 setuptools.setup(
     name="pangres",
-    version="2.2.3",
+    version="2.2.4",
     license = 'The Unlicense',
     author="Thibault Bétrémieux",
     author_email="thibault.betremieux@gmail.com",
     url = 'https://github.com/ThibTrip/pangres',
-    download_url = 'https://github.com/ThibTrip/pangres/archive/v2.2.3.tar.gz',
+    download_url = 'https://github.com/ThibTrip/pangres/archive/v2.2.4.tar.gz',
     keywords = ['pandas','postgres', 'mysql', 'sqlite'],
     description=description,
     long_description=long_description,
