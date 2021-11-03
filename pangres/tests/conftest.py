aa75364aa7078e611448914daf822301d83e1730
Thibault Betremieux <thibault.betremieux@port-neo.com>
2021-11-03 15:23:23 +0100

    tests(conftest): add testy generation for async engines and improve test ids

--- pangres/tests/conftest.py
+++ pangres/tests/conftest.py
@@ -5,6 +5,7 @@
 Configuration and helpers for the tests of pangres with pytest.
 """
 import pandas as pd
+import pytest
 import json
 from sqlalchemy import create_engine, MetaData, Table, text
 from sqlalchemy import inspect as sqla_inspect
@@ -15,6 +16,40 @@ from pangres.helpers import _sqla_gt14
 # -
 
 # # Helpers
+
+# ## ConnectionString object
+
+class ConnectionString:
+    def __init__(self, string, is_async=False, schema=None):
+        self._string = string
+        self._is_async = is_async
+        self._schema = schema
+
+    @property
+    def string(self):
+        return self._string
+
+    @property
+    def is_async(self):
+        return self._is_async
+
+    @property
+    def schema(self):
+        return self._schema
+
+
+    def get_engine(self, **kwargs):
+        if not self._is_async:
+            return create_engine(self._string, **kwargs)
+        else:
+            if not _sqla_gt14():
+                from sqlalchemy import __version__ as sqla_version
+                raise NotImplementedError('Async engines require sqlalchemy >= 1.4. '
+                                          f'Current version is {sqla_version}')
+            else:
+                from sqlalchemy.ext.asyncio import create_async_engine
+                return create_async_engine(self._string, **kwargs)
+
 
 # ## Functions for dropping a table
 
@@ -66,37 +101,70 @@ async def adrop_table_if_exists(engine, schema, table_name):
         await connection.run_sync(_drop_table_from_conn, schema=schema, table_name=table_name)
         await connection.commit()
 
+
+# -
+
+# # Pytest functions
+
 # +
 def pytest_addoption(parser):
     parser.addoption('--sqlite_conn', action="store", default=None)
     parser.addoption('--pg_conn', action="store", default=None)
     parser.addoption('--mysql_conn', action="store", default=None)
+    parser.addoption('--pg_conn_async', action="store", default=None)
+    parser.addoption('--mysql_conn_async', action="store", default=None)
     parser.addoption('--pg_schema', action='store', default=None)
 
+# this is called for every test
 def pytest_generate_tests(metafunc):
-    # This is called for every test. Only get/set command line arguments
-    # if the argument is specified in the list of test "fixturenames".
-    conn_strings = {'sqlite':metafunc.config.option.sqlite_conn,
-                    'pg':metafunc.config.option.pg_conn,
-                    'mysql':metafunc.config.option.mysql_conn}
-    engines = []
-    schemas = []
-    for db_type, conn_string in conn_strings.items():
-        if conn_string is None:
-            continue
-        schema = metafunc.config.option.pg_schema if db_type == 'pg' else None
-        engine = create_engine(conn_string)
-        schemas.append(schema)
-        engines.append(engine)
-        # for sqlalchemy 1.4+ use future=True to try the future sqlalchemy 2.0
-        if _sqla_gt14():
-            future_engine = create_engine(conn_string, future=True)
-            schemas.append(schema)
-            engines.append(future_engine)
-    assert len(engines) == len(schemas)
-    if len(engines) == 0:
+    options = metafunc.config.option
+    pg_schema = options.pg_schema if options.pg_schema is not None else 'public'
+    conn_strings = [ConnectionString(string=options.sqlite_conn),
+                    ConnectionString(string=options.pg_conn, schema=pg_schema),
+                    ConnectionString(string=options.mysql_conn),
+                    ConnectionString(string=options.pg_conn_async, is_async=True, schema=pg_schema),
+                    ConnectionString(string=options.mysql_conn_async, is_async=True)]
+
+    # filter connection strings (not provided ones will be None)
+    conn_strings = [c for c in conn_strings if c.string is not None]
+    if len(conn_strings) == 0:
         raise ValueError('You must provide at least one connection string (e.g. argument --sqlite_conn)!')
-    metafunc.parametrize("engine, schema", list(zip(engines, schemas)), scope='module')
+
+    # prepare parameters for tests
+    engines, schemas, ids = [], [], []
+    is_async_test_module = metafunc.module.__name__.endswith('_async')
+    for conn_string in conn_strings:
+        # skip sync tests for async engines
+        if conn_string.is_async and not is_async_test_module:
+            continue
+        # skip async tests for sync engines
+        if not conn_string.is_async and is_async_test_module:
+            continue
+
+        # get engine and schemas that we will use as parameters for all tests
+        engine, schema = conn_string.get_engine(), conn_string.schema
+        engines.append(engine)
+        schemas.append(schema)
+        ids.append(f'{engine.url.drivername}_{schema}')
+
+        # for sqlalchemy 1.4+ use future=True to try the future sqlalchemy 2.0
+        # (only for non async engines)
+        if _sqla_gt14() and not conn_string.is_async:
+            future_engine = conn_string.get_engine(future=True)
+            engines.append(future_engine)
+            schemas.append(schema)
+            ids.append(f'{future_engine.url.drivername}_FUTURE_{schema}')
+
+    # verifications
+    assert len(engines) == len(schemas) == len(ids)
+
+    # handle case when there is nothing to test
+    if len(engines) == 0:
+        pytest.skip()
+
+    # generate tests
+    params = list(zip(engines, schemas))
+    metafunc.parametrize("engine, schema", params, ids=ids, scope='module')
 
 
 # -
