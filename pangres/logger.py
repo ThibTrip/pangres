c494c95a077bd4e17e1307b1d60e38844f1401e7
Thibault Bétrémieux <thibault.betremieux@port-neo.com>
2021-12-05 19:15:19 +0100

    fix(logger): it seems the logger always used the info level...

--- pangres/logger.py
+++ pangres/logger.py
@@ -1,13 +1,23 @@
 # +
+"""
+Module heavily inspired from:
+
+https://github.com/SergeyPirogov/webdriver_manager/blob/master/webdriver_manager/logger.py
+"""
 import logging
 import os
 
 loggers = {}
+log_method_switch = {logging.CRITICAL:'critical', # same level as fatal
+                     logging.ERROR:'error',
+                     logging.WARNING:'warning',
+                     logging.INFO:'info',
+                     logging.DEBUG:'debug'}
+
 
 def log(text, name='pangres', level=logging.INFO):
     """
     Logs given text to stderr (default of the logging library).
-
     Parameters
     ----------
     text : str
@@ -21,21 +31,34 @@ def log(text, name='pangres', level=logging.INFO):
     This is heavily inspired from:
     https://github.com/SergeyPirogov/webdriver_manager/blob/master/webdriver_manager/logger.py
 
+    I wanted to do it with two functions as well (_init_handler and log) but could not get
+    it to work somehow
+
     Examples
     --------
-    >>> log('info')
-
-    >>> import logging
-    >>> log('warning!', level=logging.WARNING)
+    * setting the log level of pangres via an environment variable
+    >>> import os, logging
+    >>> from pangres.logger import log
+    >>> os.environ['PANGRES_LOG_LEVEL'] = str(logging.WARNING) # doctest: +SKIP
+    >>>
+    >>> # this won't log anything (INFO level < WARNING level)
+    >>> log('info', level=logging.INFO) # doctest: +SKIP
+    >>>
+    >>> # this will log something
+    >>> log('warn', level=logging.WARNING) # doctest: +SKIP
     """
+    # get the appropriate log method (info, warning etc.)
+    try:
+        log_method = log_method_switch[level]
+    except KeyError:
+        raise ValueError(f'{level} is not a valid log level. See https://docs.python.org/3/library/logging.html')
+
     # environment variable so user can customize the logging level of pangres
-    log_level = os.getenv('PANGRES_LOG_LEVEL')
-    if log_level:
-        level = int(log_level)
-    # only add a handler if we do not have one (otherwise we'd have duplicated logs)
-    if loggers.get(name):
-        loggers.get(name).info(text)
-    else:
+    logger_level = os.getenv('PANGRES_LOG_LEVEL', logging.INFO)
+    logger_level = int(logger_level) if isinstance(logger_level, str) else logger_level
+
+    # init logger
+    if name not in loggers:
         _logger = logging.getLogger(name)
         handler = logging.StreamHandler()
         formatter = logging.Formatter('%(asctime)s | %(levelname)s     '
@@ -43,6 +66,8 @@ def log(text, name='pangres', level=logging.INFO):
                                       '- %(message)s')
         handler.setFormatter(formatter)
         _logger.addHandler(handler)
-        _logger.setLevel(level)
+        _logger.setLevel(logger_level)
         loggers[name] = _logger
-        _logger.info(text)
+
+    # log
+    getattr(loggers[name], log_method)(text)
