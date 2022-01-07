f011aa86435902761e069e8c6a8ea247d8902d53
Thibault Betremieux <thibault.betremieux@port-neo.com>
2022-01-07 14:01:24 +0100

    feat: add upsert_future (with first parameter "con" instead of "engine")

--- pangres/__init__.py
+++ pangres/__init__.py
@@ -1,4 +1,4 @@
-from pangres.core import upsert
+from pangres.core import upsert, upsert_future
 from pangres.utils import fix_psycopg2_bad_cols
 from pangres.examples import DocsExampleTable
 from pangres._version import __version__
