649b098cdfae6032b591231bfd105ee3e2e28451
Thibault Bétrémieux <40694343+ThibTrip@users.noreply.github.com>
2023-01-29 13:18:53 +0100

    chore(setup): add Python 3.11 to the list of compatible versions

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setuptools.setup(
                  "Programming Language :: Python :: 3.8",
                  "Programming Language :: Python :: 3.9",
                  "Programming Language :: Python :: 3.10",
+                 "Programming Language :: Python :: 3.11",
                  "License :: Public Domain",
                  "Intended Audience :: Developers",
                  "Operating System :: OS Independent"])
